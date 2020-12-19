5631e4b994e2ff39ee85c46ecdbf8c2908e8b293
eevi-inkeri.laakso <eevi-inkeri.laakso@aktia.fi>
2020-12-19 12:05:40 +0200

    remove list sort

--- day5/part2.py
+++ day5/part2.py
@@ -1,9 +1,8 @@
 from part1 import read_file_to_list, get_all_seat_ids
 
 
-def find_my_seat(seat_ids, max_seat_id):
-    seat_ids.sort()
-    for id in range(max_seat_id):
+def find_my_seat(seat_ids):
+    for id in range(len(seat_ids)):
         if (id + 1) in seat_ids and (id - 1) in seat_ids and id not in seat_ids:
             return id
 
@@ -11,8 +10,8 @@ def find_my_seat(seat_ids, max_seat_id):
 def main():
     boarding_passes = read_file_to_list('input5.txt')
     seat_ids = get_all_seat_ids(boarding_passes)
-    max_seat_id = max(seat_ids)
-    my_seat_id = find_my_seat(seat_ids, max_seat_id)
+
+    my_seat_id = find_my_seat(seat_ids)
     print(my_seat_id)
 
 
