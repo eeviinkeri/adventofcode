e08ff4343a49e91cbaee91761745c6ed207a3983
eevi-inkeri.laakso <eevi-inkeri.laakso@aktia.fi>
2020-12-19 10:08:30 +0200

    add day4 part1

---
+++ day4/part1.py
@@ -0,0 +1,29 @@
+def validate_required_fields(passport):
+    required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    counter = 0
+    for field in required_fields:
+        if field in passport:
+            counter += 1
+    if counter == 7:
+        return True 
+
+
+def read_file_to_list(filename):
+    with open(filename, "r") as f:
+        return f.read().replace('\n', ' ').split('  ')
+
+
+def main():
+    data = read_file_to_list('input4.txt')
+    print(data)
+
+    valid_entries = 0
+    for passport in data:
+        if validate_required_fields(passport):               
+            valid_entries += 1
+
+    print(valid_entries)
+
+
+if __name__ == "__main__":
+    main()
