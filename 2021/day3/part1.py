71392020649e4b340c5877b152fb4434dc12fe66
Eevi-Inkeri Laakso <eeviinkeri@gmail.com>
2021-12-06 04:41:59 +0200

    refactor for part 2

--- 2021/day3/part1.py
+++ 2021/day3/part1.py
@@ -1,3 +1,13 @@
+def get_least_common_bits(most_common_bits):
+    least_common_bits = []
+    for bit in most_common_bits:
+        if bit == 1:
+            least_common_bits.append(0)
+        else:
+            least_common_bits.append(1)
+    return least_common_bits
+
+
 def count_bits(value, bit_list):
     values = []
     for bit in bit_list:
@@ -16,6 +26,14 @@ def find_most_common_bit(position, diagnostinc_report):
         return 0
 
 
+def get_most_common_bits(diagnostic_report):
+    most_common_bits = []
+    for position in range(0, 12):
+        most_commont_bit = find_most_common_bit(position, diagnostic_report)
+        most_common_bits.append(most_commont_bit)
+    return most_common_bits
+
+
 def read_lines_in_file_to_list(filename: str):
     with open(filename, "r") as f:
         data = [line.strip("\n") for line in f]
@@ -23,19 +41,10 @@ def read_lines_in_file_to_list(filename: str):
 
 
 def main():
-    most_common_bits = []
-    least_common_bits = []
-
     diagnostic_report = read_lines_in_file_to_list('day3/input.txt')
-    for position in range(0, 12):
-        most_commont_bit = find_most_common_bit(position, diagnostic_report)
-        most_common_bits.append(most_commont_bit)
 
-    for bit in most_common_bits:
-        if bit == 1:
-            least_common_bits.append(0)
-        else:
-            least_common_bits.append(1)
+    most_common_bits = get_most_common_bits(diagnostic_report)
+    least_common_bits = get_least_common_bits(most_common_bits)
 
     # convert list of integers to binary number and then to integer
     gamma_rate = int("".join(str(x) for x in most_common_bits), 2)
