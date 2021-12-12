f1f5820ecc9b3951beb6313976d3bc815ed0d099
Eevi-Inkeri Laakso <eeviinkeri@gmail.com>
2021-12-12 07:42:54 +0200

    refactor day 3 part 1

--- 2021/day3/part1.py
+++ 2021/day3/part1.py
@@ -1,11 +1,5 @@
-def get_least_common_bits(most_common_bits):
-    least_common_bits = []
-    for bit in most_common_bits:
-        if bit == 1:
-            least_common_bits.append(0)
-        else:
-            least_common_bits.append(1)
-    return least_common_bits
+def convert_binary_to_integer(binary_as_list_of_int):
+    return int("".join(str(x) for x in binary_as_list_of_int), 2)
 
 
 def count_bits(value, bit_list):
@@ -14,6 +8,24 @@ def count_bits(value, bit_list):
         if bit == value:
             values.append(bit)
     return len(values)
+
+
+def find_least_common_bit(position, diagnostinc_report):
+    bit_list = [int(report[position]) for report in diagnostinc_report]
+    ones = count_bits(1, bit_list)
+    zeros = count_bits(0, bit_list)
+    if ones >= zeros:
+        return 0
+    else:
+        return 1
+
+
+def get_least_common_bits(diagnostic_report):
+    least_common_bits = []
+    for position in range(0, 12):
+        least_commont_bit = find_least_common_bit(position, diagnostic_report)
+        least_common_bits.append(least_commont_bit)
+    return least_common_bits
 
 
 def find_most_common_bit(position, diagnostinc_report):
@@ -44,11 +56,10 @@ def main():
     diagnostic_report = read_lines_in_file_to_list('day3/input.txt')
 
     most_common_bits = get_most_common_bits(diagnostic_report)
-    least_common_bits = get_least_common_bits(most_common_bits)
+    least_common_bits = get_least_common_bits(diagnostic_report)
 
-    # convert list of integers to binary number and then to integer
-    gamma_rate = int("".join(str(x) for x in most_common_bits), 2)
-    epsilon_rate = int("".join(str(x) for x in least_common_bits), 2)
+    gamma_rate = convert_binary_to_integer(most_common_bits)
+    epsilon_rate = convert_binary_to_integer(least_common_bits)
 
     power_consumption = gamma_rate * epsilon_rate
     print(f'power consumption of the submarine is {power_consumption}')
