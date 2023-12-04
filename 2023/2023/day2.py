2cddfa528ae1be60d8b40031025fc8240729182e
Eevi-Inkeri Laakso <eeviinkeri@gmail.com>
2023-12-04 20:32:18 +0200

    day2 part2

--- 2023/2023/day2.py
+++ 2023/2023/day2.py
@@ -1,15 +1,53 @@
 import os
 import re
 
-'''
-Determine which games would have been possible if the bag had been loaded with only 
-12 red cubes, 13 green cubes, and 14 blue cubes
-'''
 
 class Game:
     def __init__(self, number, rounds):
         self.number = number
         self.rounds = rounds
+
+
+def find_possible_games(games):
+    '''
+    Determine which games would have been possible if the bag had been loaded with only
+    12 red cubes, 13 green cubes, and 14 blue cubes
+    '''
+    red_cubes = 12
+    green_cubes = 13
+    blue_cubes = 14
+    impossible_games = list()
+    for game in games:
+        for round in game.rounds:
+            if round['red'] > red_cubes or round['blue'] > blue_cubes or round['green'] > green_cubes:
+                impossible_games.append(game.number)
+    impossible_set = set(impossible_games)
+    possible_games = list()
+    for i in range(1, 101):
+        if i not in impossible_set:
+            possible_games.append(i)
+    return possible_games
+
+
+def find_min_cube_count(games):
+    '''
+    what is the fewest number of cubes of each color that could have been
+    in the bag to make the game possible?
+    '''
+    min_cube_powers = list()
+    for game in games:
+        red = 0
+        blue = 0
+        green = 0
+        for round in game.rounds:
+            if round['red'] > red:
+                red = round['red']
+            if round['blue'] > blue:
+                blue = round['blue']
+            if round['green'] > green:
+                green = round['green']
+        min_cube_powers.append(red*blue*green)
+    return min_cube_powers
 
 
 def read_file_to_list(filename):
@@ -21,10 +59,6 @@ def read_file_to_list(filename):
 def main():
     filename = f"{os.getcwd()}/data/input2.txt"
     colors = ["red", "blue", "green"]
-    red_cubes = 12
-    green_cubes = 13
-    blue_cubes = 14
-
     data = read_file_to_list(filename)
 
     # parse games
@@ -51,20 +85,12 @@ def main():
             else: round["green"] = 0
             rounds.append(round)
         games.append(Game(number=game_id, rounds=rounds))
-    
-    # find possible games
-    impossible_games = list()
-    for game in games:
-        for round in game.rounds:
-            if round['red'] > red_cubes or round['blue'] > blue_cubes or round['green'] > green_cubes:
-                impossible_games.append(game.number)
-    impossible_set = set(impossible_games)
-    possible_games = list()
-    for i in range(1, 101):
-        if i not in impossible_set: 
-            possible_games.append(i)
-    print(sum(possible_games))
 
+    possible_games = find_possible_games(games)
+    print("Answer to day2 part one is:", sum(possible_games))
+
+    power_of_min_cube_set = find_min_cube_count(games)
+    print("Answer to day2 part two is:", sum(power_of_min_cube_set))
 
 if __name__ == "__main__":
     main()
